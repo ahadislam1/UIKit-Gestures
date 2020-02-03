0689bfeabbbd341e13874a07ee7e380d3458d3e6
Ahad Islam <ahad.islam624@gmail.com>
2020-02-03 12:44:29 -0500

    Initial Commit

---
+++ UIKit-GesturesTests/UIKit_GesturesTests.swift
@@ -0,0 +1,34 @@
+//
+//  UIKit_GesturesTests.swift
+//  UIKit-GesturesTests
+//
+//  Created by Ahad Islam on 2/3/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import XCTest
+@testable import UIKit_Gestures
+
+class UIKit_GesturesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
